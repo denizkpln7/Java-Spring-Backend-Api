d7bd80c271d881be6a9d03e0b3f7fb860b279ccb
denizkpln7 <84469468+denizkpln7@users.noreply.github.com>
2022-07-17 18:11:36 +0300

    first commit

---
+++ BackendApi/src/main/java/com/works/config/Util.java
@@ -0,0 +1,22 @@
+package com.works.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Util {
+
+
+    @Bean
+    public CacheManager cacheManager() {
+        // configure and return an implementation of Spring's CacheManager SPI
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("productList")));
+        return cacheManager;
+    }
+}
